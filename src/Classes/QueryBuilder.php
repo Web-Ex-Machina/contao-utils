e38f72732847f7ea04d8ea09dbef6fef451b3c27
LupusVII <jln.thirion@gmail.com>
2020-05-23 17:38:03 +0200

    Add Models Utilities

---
+++ src/Classes/QueryBuilder.php
@@ -0,0 +1,142 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Contao Utilities for Contao Open Source CMS
+ * Copyright (c) 2019-2020 Web ex Machina
+ *
+ * @category ContaoBundle
+ * @package  Web-Ex-Machina/contao-utils
+ * @author   Web ex Machina <contact@webexmachina.fr>
+ * @link     https://github.com/Web-Ex-Machina/contao-utils/
+ */
+
+namespace WEM\UtilsBundle\Classes;
+
+use Contao\Database;
+use Contao\DcaExtractor;
+
+class QueryBuilder
+{
+    /**
+     * Build a query based on the given options.
+     *
+     * @param array $arrOptions The options array
+     *
+     * @return string The query string
+     */
+    public static function find(array $arrOptions)
+    {
+        $objBase = DcaExtractor::getInstance($arrOptions['table']);
+
+        $strSelect = '*';
+        if ($arrOptions['select']) {
+            if (\is_array($arrOptions['select'])) {
+                $strSelect = implode(',', $arrOptions['select']);
+            } else {
+                $strSelect = $arrOptions['select'];
+            }
+        }
+        $strJoin = '';
+        if ($arrOptions['join']) {
+            $strJoin = implode('', $arrOptions['join']);
+        }
+
+        if (!$objBase->hasRelations()) {
+            $strQuery = 'SELECT '.$strSelect.' FROM '.$arrOptions['table'].$strJoin;
+        } else {
+            $arrJoins = [];
+            if ('*' === $strSelect) {
+                $arrFields = [$arrOptions['table'].'.*'];
+            } else {
+                $arrFields = [$strSelect];
+            }
+
+            if ('' !== $strJoin) {
+                $arrJoins[] = $strJoin;
+            }
+
+            $intCount = 0;
+
+            foreach ($objBase->getRelations() as $strKey => $arrConfig) {
+                // Automatically join the single-relation records
+                if ('eager' === $arrConfig['load'] || $arrOptions['eager']) {
+                    if ('hasOne' === $arrConfig['type'] || 'belongsTo' === $arrConfig['type']) {
+                        ++$intCount;
+                        $objRelated = DcaExtractor::getInstance($arrConfig['table']);
+
+                        foreach (array_keys($objRelated->getFields()) as $strField) {
+                            $arrFields[] = 'j'.$intCount.'.'.Database::quoteIdentifier($strField).' AS '.$strKey.'__'.$strField;
+                        }
+
+                        $arrJoins[] = ' LEFT JOIN '.$arrConfig['table']." j$intCount ON ".$arrOptions['table'].'.'.Database::quoteIdentifier($strKey)."=j$intCount.".$arrConfig['field'];
+                    }
+                }
+            }
+
+            // Generate the query
+            $strQuery = 'SELECT '.implode(', ', $arrFields).' FROM '.$arrOptions['table'].' '.implode(' ', $arrJoins);
+        }
+
+        // Where condition
+        if (isset($arrOptions['column'])) {
+            $strQuery .= ' WHERE '.(\is_array($arrOptions['column']) ? implode(' AND ', $arrOptions['column']) : $arrOptions['table'].'.'.Database::quoteIdentifier($arrOptions['column']).'=?');
+        }
+
+        // Group by
+        if (isset($arrOptions['group'])) {
+            if (\is_array($arrOptions['group'])) {
+                $strQuery .= ' GROUP BY '.implode(',', $arrOptions['group']);
+            } else {
+                $strQuery .= ' GROUP BY '.$arrOptions['group'];
+            }
+        }
+
+        // Having (see #6446)
+        if (isset($arrOptions['having'])) {
+            $strQuery .= ' HAVING '.$arrOptions['having'];
+        }
+
+        // Order by
+        if (isset($arrOptions['order'])) {
+            if (\is_array($arrOptions['order'])) {
+                $strQuery .= ' ORDER BY '.implode(',', $arrOptions['order']);
+            } else {
+                $strQuery .= ' ORDER BY '.$arrOptions['order'];
+            }
+        }
+
+        if (true === $arrOptions['debug']) {
+            print_r($strQuery);
+            die;
+        }
+
+        return $strQuery;
+    }
+
+    /**
+     * Build a query based on the given options to count the number of records.
+     *
+     * @param array $arrOptions The options array
+     *
+     * @return string The query string
+     */
+    public static function count(array $arrOptions)
+    {
+        $strQuery = 'SELECT COUNT(*) AS count FROM '.$arrOptions['table'];
+
+        if (null !== $arrOptions['column']) {
+            $strQuery .= ' WHERE '.(\is_array($arrOptions['column']) ? implode(' AND ', $arrOptions['column']) : $arrOptions['table'].'.'.Database::quoteIdentifier($arrOptions['column']).'=?');
+        }
+
+        if (true === $arrOptions['debug']) {
+            print_r($strQuery);
+            die;
+        }
+
+        return $strQuery;
+    }
+}
+
+class_alias(QueryBuilder::class, 'Model\QueryBuilder');
---
+++ src/Model/Model.php
@@ -0,0 +1,267 @@
+<?php
+
+/**
+ * Leads Bundle for Contao Open Source CMS
+ * Copyright (c) 2019 Web ex Machina
+ * php version 7.3.
+ *
+ * @category ContaoBundle
+ *
+ * @author   Web ex Machina <contact@webexmachina.fr>
+ * @license  https://choosealicense.com/licenses/agpl-3.0/ GNU AGPLv3
+ *
+ * @see     https://www.webexmachina.fr
+ */
+
+namespace WEM\UtilsBundle\Model;
+
+use RuntimeException as Exception;
+use Contao\Model;
+use WEM\UtilsBundle\Classes\QueryBuilder;
+
+/**
+ * Reads and writes items.
+ */
+abstract class Model extends Contao\Model
+{
+    /**
+     * Find items, depends on the arguments.
+     *
+     * @param array $arrConfig  [Request Config]
+     * @param int   $intLimit   [Query Limit]
+     * @param int   $intOffset  [Query Offset]
+     * @param array $arrOptions [Query Options]
+     *
+     * @return Collection
+     */
+    public static function findItems($arrConfig = [], $intLimit = 0, $intOffset = 0, array $arrOptions = [])
+    {
+        try {
+            $t = static::$strTable;
+            $arrColumns = static::formatColumns($arrConfig);
+
+            if ($intLimit > 0) {
+                $arrOptions['limit'] = $intLimit;
+            }
+
+            if ($intOffset > 0) {
+                $arrOptions['offset'] = $intOffset;
+            }
+
+            if (!isset($arrOptions['order'])) {
+                $arrOptions['order'] = "$t.createdAt DESC";
+            }
+
+            if (empty($arrColumns)) {
+                return static::findAll($arrOptions);
+            } else {
+                return static::findBy($arrColumns, null, $arrOptions);
+            }
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * Count items, depends on the arguments.
+     *
+     * @param array [Request Config]
+     * @param array [Query Options]
+     *
+     * @return int
+     */
+    public static function countItems($arrConfig = [], array $arrOptions = [])
+    {
+        try {
+            $t = static::$strTable;
+            $arrColumns = static::formatColumns($arrConfig);
+
+            if (empty($arrColumns)) {
+                return static::countAll($arrOptions);
+            } else {
+                return static::countBy($arrColumns, null, $arrOptions);
+            }
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * Format ItemModel columns.
+     *
+     * @param array $arrConfig [Configuration to format]
+     *
+     * @return array
+     */
+    public static function formatColumns($arrConfig)
+    {
+        try {
+            $t = static::$strTable;
+            $arrColumns = [];
+
+            foreach ($arrConfig as $c => $v) {
+                $arrColumns = array_merge($arrColumns, parent::formatStatement($c, $v));
+            }
+
+            if ($arrConfig['not']) {
+                $arrColumns[] = $arrConfig['not'];
+            }
+
+            return $arrColumns;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * Format Search statement
+     *
+     * @param string $strColumn    [Column to format]
+     * @param string $varValue     [Value to use]
+     *
+     * @return String
+     */
+    public static function formatSearchStatement($strField, $varValue) {
+        $t = static::$strTable;
+
+        switch($strField) {
+            default:
+                return "$t.$strField REGEXP '$varValue'";
+        }
+    }
+
+    /**
+     * Generic statements format.
+     *
+     * @param string $strField    [Column to format]
+     * @param mixed  $varValue    [Value to use]
+     * @param string $strOperator [Operator to use, default "="]
+     *
+     * @return array
+     */
+    public static function formatStatement($strField, $varValue, $strOperator = '=')
+    {
+        $arrColumns = [];
+        $t = static::$strTable;
+
+        switch ($strField) {
+            // Integer fields
+            case 'pid':
+                $arrColumns[] = sprintf("$t.%s = %s", $strField, $varValue);
+                break;
+
+            // Search in table
+            case 'search':
+                $arrSearchColumns = [];
+                if (is_array($varValue) && array_key_exists('column', $varValue)) {
+                    $arrSearchColumns[] = $varValue['column'];
+                    $arrSearchKeywords = $varValue['keywords'];
+                } elseif (static::$arrSearchFields) {
+                    $arrSearchColumns = static::$arrSearchFields;
+                    $arrSearchKeywords = $varValue;
+                } else {
+                    break;
+                }
+
+                if (!empty($arrSearchColumns)) {
+                    $k = implode('|', $arrSearchKeywords);
+                    $arrKeywords = [];
+                    foreach ($arrSearchColumns as $f) {
+                        $arrKeywords[] = static::formatSearchStatement($f, $k);
+                    }
+                    $arrColumns[] = '('.implode(' OR ', $arrKeywords).')';
+                }
+
+                break;
+
+            // Wizard for active items
+            case 'active':
+                if (1 == $varValue) {
+                    $arrColumns[] = "$t.isActive = 1 AND ($t.isActiveAt = 0 OR $t.isActiveAt <= ".time().") AND ($t.isActiveUntil = 0 OR $t.isActiveUntil >= ".time().')';
+                } elseif (-1 == $varValue) {
+                    $arrColumns[] = "$t.isActive = '' AND ($t.isActiveAt = 0 OR $t.isActiveAt >= ".time().") AND ($t.isActiveUntil = 0 OR $t.isActiveUntil <= ".time().')';
+                }
+                break;
+
+            case 'invisible':
+                if (1 == $varValue) {
+                    $arrColumns[] = "$t.invisible = 1";
+                } elseif (-1 == $varValue) {
+                    $arrColumns[] = "$t.invisible = ''";
+                }
+                break;
+
+            // Checkboxes
+            case 'isActive':
+                if (1 == $varValue) {
+                    $arrColumns[] = "$t.$strField = 1";
+                } elseif (-1 == $varValue) {
+                    $arrColumns[] = "$t.$strField = ''";
+                }
+                break;
+
+            // Dates
+            case 'tstamp':
+            case 'createdAt':
+            case 'isActiveAt':
+            case 'isActiveUntil':
+                $arrColumns[] = sprintf("$t.%s %s %s", $strField, $strOperator, $varValue);
+                break;
+
+            // Inline wheres
+            case 'where':
+                $arrColumns = array_merge($arrColumns, $varValue);
+                break;
+
+            // Default behaviour
+            case 'ptable':
+            default:
+                $arrColumns[] = sprintf("$t.%s %s '%s'", $strField, $strOperator, $varValue);
+        }
+
+        return $arrColumns;
+    }
+
+    /**
+     * Find only the items available, for filters.
+     *
+     * @param string $strField [Column]
+     *
+     * @return Collection
+     */
+    public static function findItemsGroupByOneField($strField)
+    {
+        try {
+            $t = static::$strTable;
+            $objResults = \Database::getInstance()->prepare("SELECT $t.$strField FROM $t GROUP BY $t.$strField")->execute();
+
+            return $objResults;
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * Build a query based on the given options
+     *
+     * @param array $arrOptions The options array
+     *
+     * @return string The query string
+     */
+    protected static function buildFindQuery(array $arrOptions)
+    {
+        return QueryBuilder::find($arrOptions);
+    }
+
+    /**
+     * Build a query based on the given options to count the number of records
+     *
+     * @param array $arrOptions The options array
+     *
+     * @return string The query string
+     */
+    protected static function buildCountQuery(array $arrOptions)
+    {
+        return QueryBuilder::count($arrOptions);
+    }
+}
