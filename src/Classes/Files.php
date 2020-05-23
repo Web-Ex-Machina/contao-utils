b2cd7b42f22aa0bf6f18db920579044742a3c3b4
LupusVII <jln.thirion@gmail.com>
2020-05-23 17:49:42 +0200

    Add a helper to convert base64 files to Contao files

---
+++ src/Classes/Files.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Contao Utilities for Contao Open Source CMS
+ * Copyright (c) 2019-2020 Web ex Machina
+ *
+ * @category ContaoBundle
+ * @package  Web-Ex-Machina/contao-utils
+ * @author   Web ex Machina <contact@webexmachina.fr>
+ * @link     https://github.com/Web-Ex-Machina/contao-utils/
+ */
+
+namespace WEM\UtilsBundle\Classes;
+
+class Files
+{
+	/**
+     * Contao Friendly Base64 Converter to FileSystem.
+     *
+     * @param [String] $folder [Folder name]
+     * @param [String] $file   [File name]
+     *
+     * @return [Object] [File Object]
+     */
+    public static function base64ToImage($data, $folder, $file)
+    {
+        if (preg_match('/^data:image\/(\w+);base64,/', $data, $type)) {
+            $data = substr($data, strpos($data, ',') + 1);
+            $type = strtolower($type[1]); // jpg, png, gif
+
+            if (false === strpos(\Config::get('validImageTypes'), $type)) {
+                return false;
+            }
+
+            $data = str_replace(' ', '+', $data);
+            $data = base64_decode($data);
+
+            if (false === $data) {
+                throw new \Exception('base64_decode failed');
+            }
+
+            $path = $folder.'/'.$file.'.'.$type;
+
+            // open the output file for writing
+            $objFile = new \File($path);
+            $objFile->truncate();
+            $objFile->write($data);
+            $objFile->close();
+
+            return $objFile;
+        }
+        throw new \Exception('did not match data URI with image data');
+    }
+}
