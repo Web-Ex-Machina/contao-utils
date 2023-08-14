f363b902ac0dba1502be2660bbafd20ad27a8050
LupusVII <jln.thirion@gmail.com>
2023-08-15 00:06:22 +0200

    Add a function to convert bits to a readable filesize format

--- src/Classes/StringUtil.php
+++ src/Classes/StringUtil.php
@@ -90,4 +90,41 @@ class StringUtil extends \Contao\StringUtil
 
         return array_values($arrKeywords);
     }
+
+    /**
+     * Convert a number of bits to a readable filesize format
+     *
+     * @param int $filesize
+     *
+     * @return string
+     */
+    public static function formatFileSize($bytes)
+    {
+        if ($bytes >= 1073741824)
+        {
+            $bytes = number_format($bytes / 1073741824, 2) . ' GB';
+        }
+        elseif ($bytes >= 1048576)
+        {
+            $bytes = number_format($bytes / 1048576, 2) . ' MB';
+        }
+        elseif ($bytes >= 1024)
+        {
+            $bytes = number_format($bytes / 1024, 2) . ' KB';
+        }
+        elseif ($bytes > 1)
+        {
+            $bytes = $bytes . ' bytes';
+        }
+        elseif ($bytes == 1)
+        {
+            $bytes = $bytes . ' byte';
+        }
+        else
+        {
+            $bytes = '0 bytes';
+        }
+
+        return $bytes;
+    }
 }
