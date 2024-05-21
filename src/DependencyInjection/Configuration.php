665bdafe17cf2916382637240a337f842234181e
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2024-05-21 12:10:52 +0200

    remove doc

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,14 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- * Encryption extension for Contao Open Source CMS
- *
- * @copyright     Copyright (c) 2022, Christian Barkowsky & Christoph Werner
- * @author        Christian Barkowsky <https://plenta.io/>
- * @author        Christoph Werner <https://plenta.io/>
- */
-
 namespace WEM\UtilsBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
