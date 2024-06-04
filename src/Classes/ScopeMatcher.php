7dcc8064c46b53ea729e403c12f122b261b177f0
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2024-06-04 11:39:58 +0200

    feat: Add ScopeMatcher class and configure its service

---
+++ src/Classes/ScopeMatcher.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Contao Utilities for Contao Open Source CMS
+ * Copyright (c) 2019-2024 Web ex Machina
+ *
+ * @category ContaoBundle
+ * @package  Web-Ex-Machina/contao-utils
+ * @author   Web ex Machina <contact@webexmachina.fr>
+ * @link     https://github.com/Web-Ex-Machina/contao-utils/
+ */
+
+namespace WEM\UtilsBundle\Classes;
+
+use Contao\CoreBundle\Routing\ScopeMatcher as ScopeMatcherBase;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class ScopeMatcher
+{
+    private RequestStack $requestStack;
+
+    private ScopeMatcherBase $scopeMatcher;
+
+    /**
+     * I use FQDN because PHP doesn't care about the "use".
+     */
+    public function __construct( RequestStack $requestStack, ScopeMatcherBase $scopeMatcher) {
+        $this->requestStack = $requestStack;
+        $this->scopeMatcher = $scopeMatcher;
+    }
+
+    /**
+     * Returns whether the current request is a backend request.
+     *
+     * @return bool Returns true if the current request is a backend request, false otherwise.
+     */
+    public function isBackend(): bool
+    {
+        return $this->scopeMatcher->isBackendRequest($this->requestStack->getCurrentRequest());
+    }
+
+    /**
+     * Checks if the current request is a frontend request.
+     *
+     * @return bool Whether the current request is a frontend request, false otherwise.
+     */
+    public function isFrontend(): bool
+    {
+        return $this->scopeMatcher->isFrontendRequest($this->requestStack->getCurrentRequest());
+    }
+}
