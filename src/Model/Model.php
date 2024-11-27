d8e36af46602bbfbb92686be9fc55c8aa4f50c4e
LupusVII <jln.thirion@gmail.com>
2024-11-27 17:28:25 +0100

    Add default columns to the hook

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -66,7 +66,7 @@ abstract class Model extends \Contao\Model
         {
             foreach ($GLOBALS['WEM_HOOKS']['preFindItems'] as $callback)
             {
-                $arrColumns = System::importStatic($callback[0])->{$callback[1]}($t, $arrConfig, $intLimit, $intOffset, $arrOptions);
+                $arrColumns = System::importStatic($callback[0])->{$callback[1]}($t, $arrColumns, $arrConfig, $intLimit, $intOffset, $arrOptions);
             }
         }
 
